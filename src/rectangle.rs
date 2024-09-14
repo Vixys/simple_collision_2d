4ced438ddf65ecbf2748f0a10058fdd73c84ac56
Sébastien Marichal <sebmarichal@gmail.com>
2024-09-14 10:02:48 +0200

    Rectangle to rectangle collision

--- src/rectangle.rs
+++ src/rectangle.rs
@@ -23,6 +23,34 @@ impl Rectangle {
     pub fn bottom(&self) -> f32 {
         self.top - self.height
     }
+
+    pub fn top_left(&self) -> Point {
+        Point {
+            x: self.left,
+            y: self.top,
+        }
+    }
+
+    pub fn top_right(&self) -> Point {
+        Point {
+            x: self.right(),
+            y: self.top,
+        }
+    }
+
+    pub fn bottom_right(&self) -> Point {
+        Point {
+            x: self.right(),
+            y: self.bottom(),
+        }
+    }
+
+    pub fn bottom_left(&self) -> Point {
+        Point {
+            x: self.left,
+            y: self.bottom(),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -75,5 +103,53 @@ mod tests {
         };
 
         assert_eq!(r.bottom(), -10.)
+    }
+
+    #[test]
+    fn test_bottom_left() {
+        let r = Rectangle {
+            top: 0.,
+            left: 0.,
+            width: 10.,
+            height: 10.,
+        };
+
+        assert_eq!(r.bottom_left(), Point { x: 0., y: -10. })
+    }
+
+    #[test]
+    fn test_bottom_right() {
+        let r = Rectangle {
+            top: 0.,
+            left: 0.,
+            width: 10.,
+            height: 10.,
+        };
+
+        assert_eq!(r.bottom_right(), Point { x: 10., y: -10. })
+    }
+
+    #[test]
+    fn test_top_left() {
+        let r = Rectangle {
+            top: 0.,
+            left: 0.,
+            width: 10.,
+            height: 10.,
+        };
+
+        assert_eq!(r.top_left(), Point { x: 0., y: 0. })
+    }
+
+    #[test]
+    fn test_top_right() {
+        let r = Rectangle {
+            top: 0.,
+            left: 0.,
+            width: 10.,
+            height: 10.,
+        };
+
+        assert_eq!(r.top_right(), Point { x: 10., y: 0. })
     }
 }
--- src/rectangle_rectangle.rs
+++ src/rectangle_rectangle.rs
@@ -1,7 +1,10 @@
-use crate::{rectangle::Rectangle, Point};
+use crate::{point_rectangle::point_rectangle_collision, rectangle::Rectangle, Point};
 
 pub fn rectangle_rectangle_collision(r1: &Rectangle, r2: &Rectangle) -> bool {
-    false
+    point_rectangle_collision(&r1.top_left(), r2)
+        || point_rectangle_collision(&r1.top_right(), r2)
+        || point_rectangle_collision(&r1.bottom_left(), r2)
+        || point_rectangle_collision(&r1.bottom_right(), r2)
 }
 
 #[cfg(test)]
@@ -9,15 +12,215 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_point_rectangle_collision_center() {
+    fn test_rectangle_rectangle_collision_center() {
         let r = Rectangle {
+            top: 2.5,
+            left: -2.5,
+            width: 5.,
+            height: 5.,
+        };
+
+        let result = rectangle_rectangle_collision(&r, &r);
+
+        assert!(result);
+    }
+
+    #[test]
+    fn test_rectangle_rectangle_collision_collide() {
+        let r1 = Rectangle {
+            top: 2.5,
+            left: -2.5,
+            width: 5.,
+            height: 5.,
+        };
+        let r2 = Rectangle {
             top: 0.,
             left: 0.,
             width: 5.,
             height: 5.,
         };
 
-        let result = rectangle_rectangle_collision(&r, &r);
+        let result = rectangle_rectangle_collision(&r1, &r2);
+
+        assert!(result);
+    }
+
+    #[test]
+    fn test_rectangle_rectangle_collision_not_collide() {
+        let r1 = Rectangle {
+            top: 2.5,
+            left: -2.5,
+            width: 5.,
+            height: 5.,
+        };
+        let r2 = Rectangle {
+            top: 0.,
+            left: 3.,
+            width: 5.,
+            height: 5.,
+        };
+
+        let result = rectangle_rectangle_collision(&r1, &r2);
+
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_rectangle_rectangle_collision_left() {
+        let r1 = Rectangle {
+            top: 2.5,
+            left: -2.5,
+            width: 5.,
+            height: 5.,
+        };
+        let r2 = Rectangle {
+            top: 2.5,
+            left: -7.5,
+            width: 5.,
+            height: 5.,
+        };
+
+        let result = rectangle_rectangle_collision(&r1, &r2);
+
+        assert!(result);
+    }
+
+    #[test]
+    fn test_rectangle_rectangle_collision_top_left() {
+        let r1 = Rectangle {
+            top: 2.5,
+            left: -2.5,
+            width: 5.,
+            height: 5.,
+        };
+        let r2 = Rectangle {
+            top: 7.5,
+            left: -7.5,
+            width: 5.,
+            height: 5.,
+        };
+
+        let result = rectangle_rectangle_collision(&r1, &r2);
+
+        assert!(result);
+    }
+
+    #[test]
+    fn test_rectangle_rectangle_collision_bottom_left() {
+        let r1 = Rectangle {
+            top: 2.5,
+            left: -2.5,
+            width: 5.,
+            height: 5.,
+        };
+        let r2 = Rectangle {
+            top: -2.5,
+            left: -7.5,
+            width: 5.,
+            height: 5.,
+        };
+
+        let result = rectangle_rectangle_collision(&r1, &r2);
+
+        assert!(result);
+    }
+
+    #[test]
+    fn test_rectangle_rectangle_collision_right() {
+        let r1 = Rectangle {
+            top: 2.5,
+            left: -2.5,
+            width: 5.,
+            height: 5.,
+        };
+        let r2 = Rectangle {
+            top: 2.5,
+            left: 2.5,
+            width: 5.,
+            height: 5.,
+        };
+
+        let result = rectangle_rectangle_collision(&r1, &r2);
+
+        assert!(result);
+    }
+
+    #[test]
+    fn test_rectangle_rectangle_collision_top_right() {
+        let r1 = Rectangle {
+            top: 2.5,
+            left: -2.5,
+            width: 5.,
+            height: 5.,
+        };
+        let r2 = Rectangle {
+            top: 7.5,
+            left: 2.5,
+            width: 5.,
+            height: 5.,
+        };
+
+        let result = rectangle_rectangle_collision(&r1, &r2);
+
+        assert!(result);
+    }
+
+    #[test]
+    fn test_rectangle_rectangle_collision_bottom_right() {
+        let r1 = Rectangle {
+            top: 2.5,
+            left: -2.5,
+            width: 5.,
+            height: 5.,
+        };
+        let r2 = Rectangle {
+            top: -2.5,
+            left: 2.5,
+            width: 5.,
+            height: 5.,
+        };
+
+        let result = rectangle_rectangle_collision(&r1, &r2);
+
+        assert!(result);
+    }
+
+    #[test]
+    fn test_rectangle_rectangle_collision_top() {
+        let r1 = Rectangle {
+            top: 2.5,
+            left: -2.5,
+            width: 5.,
+            height: 5.,
+        };
+        let r2 = Rectangle {
+            top: 7.5,
+            left: -2.5,
+            width: 5.,
+            height: 5.,
+        };
+
+        let result = rectangle_rectangle_collision(&r1, &r2);
+
+        assert!(result);
+    }
+
+    #[test]
+    fn test_rectangle_rectangle_collision_bottom() {
+        let r1 = Rectangle {
+            top: 2.5,
+            left: -2.5,
+            width: 5.,
+            height: 5.,
+        };
+        let r2 = Rectangle {
+            top: -2.5,
+            left: -2.5,
+            width: 5.,
+            height: 5.,
+        };
+
+        let result = rectangle_rectangle_collision(&r1, &r2);
 
         assert!(result);
     }
